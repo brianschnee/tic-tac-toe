4146feb668cbd7435d088a55e4d18af9015eef49
brianschnee <brian.a.schnee@gmail.com>
2022-05-02 00:21:47 -0400

    fixed highlight with horizontal. Added border effect for win

--- js/main.js
+++ js/main.js
@@ -69,15 +69,19 @@ const TicTacToe = {
             // diagnols
             if(this.board[1][1] === char) {
                 if((this.board[0][0] === char && this.board[2][2] === char)) {
-                    this.DOMElements[0][0].style.color = 'yellow';
-                    this.DOMElements[1][1].style.color = 'yellow';
-                    this.DOMElements[2][2].style.color = 'yellow';
+                    for(let i = 0; i < 3; i++) {
+                        this.DOMElements[i][i].style.color = 'yellow';
+                        this.DOMElements[i][i].style.borderColor = 'rgb(245,199,26)';
+                    }
                     console.log('diagnol win 1')
                     return true;
                 } else if((this.board[0][2] === char && this.board[2][0] === char)) {
                     this.DOMElements[0][2].style.color = 'yellow';
+                    this.DOMElements[0][2].style.borderColor = 'rgb(245,199,26)';
                     this.DOMElements[1][1].style.color = 'yellow';
+                    this.DOMElements[1][1].style.borderColor = 'rgb(245,199,26)';
                     this.DOMElements[2][0].style.color = 'yellow';
+                    this.DOMElements[2][0].style.borderColor = 'rgb(245,199,26)';
                     console.log('diagnol win 2')
                     return true;
                 }
@@ -86,7 +90,7 @@ const TicTacToe = {
             // horizontal checks
             let location;
             let win = this.board.some((row, i) => {
-                let found = !row.some(column =>  column !== char);
+                let found = !row.some((column, j) =>  column !== char);
 
                 location = i;
                 return found;
@@ -95,8 +99,10 @@ const TicTacToe = {
             if(win) {
                 console.log('horizontal win', location)
 
-                for (let i = 0; i < 3; i++)
-                    this.DOMElements[0][i].style.color = 'yellow';
+                for (let i = 0; i < 3; i++) {
+                    this.DOMElements[location][i].style.color = 'yellow';
+                    this.DOMElements[location][i].style.borderColor = 'rgb(245,199,26)';
+                }
 
                 return win;
             }
@@ -112,8 +118,10 @@ const TicTacToe = {
                     else if(j === 2) {
                         console.log('vertical win')
 
-                        for(let k = 0; k < 3; k++)
+                        for(let k = 0; k < 3; k++) {
                             this.DOMElements[k][i].style.color = 'yellow';
+                            this.DOMElements[k][i].style.borderColor = 'rgb(245,199,26)';
+                        }
 
                         return true;
                     }
@@ -140,6 +148,10 @@ const TicTacToe = {
 
         this.board.forEach((row, i) => row.forEach((column, j) => {
             this.board[i][j] = null;
+            this.DOMElements[i][j].style.borderTop = '.8rem solid #2487d3';
+            this.DOMElements[i][j].style.borderRight = '.8rem solid #0d4066';
+            this.DOMElements[i][j].style.borderBottom = '.8rem solid #0b3454';
+            this.DOMElements[i][j].style.borderLeft = '.8rem solid #004e89';
         }));
 
         console.log(this.board);
@@ -147,7 +159,6 @@ const TicTacToe = {
         let title = document.getElementById('title')
         title.innerText = 'tic tac toe';
         title.style.fontFamily = 'Space Mono';
-
 
         console.log('%cGame has been Reset.', 'color: pink')
     }
